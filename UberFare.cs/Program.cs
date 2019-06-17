04ee5c8a25cee99f8061aa4cf74f6367b8b8a3d5
CHESERGIU <sergiu.chereches@gmail.com>
2019-06-17 23:45:37 +0300

    extract GetPricePerKm

--- UberFare.cs/Program.cs
+++ UberFare.cs/Program.cs
@@ -14,12 +14,18 @@ namespace UberFare
 
         public static decimal CalculateUberFare(int distanceInKm, int hour)
         {
-            decimal pricePerKm = 5;
-            if (IsLongDistance(distanceInKm))
-                pricePerKm = 6;
-            else if (IsMediumDistance(distanceInKm))
-                pricePerKm = 8;
+            decimal[] daytimePrices = { 5, 8, 6 };
+            decimal pricePerKm = GetPricePerKm(distanceInKm, daytimePrices);
             return distanceInKm * pricePerKm;
+        }
+
+        private static decimal GetPricePerKm(int distanceInKm, decimal[] prices)
+        {
+            if (IsLongDistance(distanceInKm))
+                return prices[2];
+            else if (IsMediumDistance(distanceInKm))
+                return prices[1];
+            return prices[0];
         }
 
         private static bool IsLongDistance(int distanceInKm)
