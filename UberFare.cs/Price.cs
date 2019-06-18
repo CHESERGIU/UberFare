c87b523658f9412550ca1666ccd5c69634810804
CHESERGIU <sergiu.chereches@gmail.com>
2019-06-18 11:19:34 +0300

    add to price riders and travels

--- UberFare.cs/Price.cs
+++ UberFare.cs/Price.cs
@@ -4,18 +4,27 @@
     {
         private readonly int distanceInKm;
         private readonly int hour;
-        private decimal[] daytimePrices = { 5, 8, 6 };
-        private decimal[] nighttimePrices = { 7, 10, 8 };
-
-        public Price(int distanceInKm, int hour)
+        private readonly decimal[] daytimePrices = { 5, 8, 6 };
+        private readonly decimal[] nighttimePrices = { 7, 10, 8 };
+        private readonly int riders;
+        private readonly int travels = 0;
+        
+        public Price(int distanceInKm, int hour, int riders, int travels)
         {
             this.distanceInKm = distanceInKm;
             this.hour = hour;
+            this.riders = riders;
+            this.travels = travels;
         }
 
-        public decimal CalculateUberFare(Price price) => distanceInKm * GetPricePerKm(price);
+        public decimal CalculateUberFare()
+        {
+            if(travels > 10)
+                return distanceInKm * GetRewardPricePerKm();
+            return distanceInKm * GetPricePerKm();
+        }
 
-        public decimal GetPricePerKm(Price price)
+        public decimal GetPricePerKm()
         {
             if (IsLongDistance(distanceInKm))
                 return GetPrices(hour)[2];
@@ -31,5 +40,20 @@
         private bool IsLongDistance(int distanceInKm) => distanceInKm > 60;
 
         private bool IsMediumDistance(int distanceInKm) => distanceInKm > 20;
+
+        public decimal GetPricePerPerson() => CalculateUberFare() / riders;
+
+        public decimal GetRewardPricePerKm()
+        {            
+            if (Fidelity10(travels))
+                return GetPrices(hour)[2]*(decimal)0.7;
+            else if (Fidelity20(travels))
+                return GetPrices(hour)[1] * (decimal)0.5;
+            return GetPrices(hour)[0];
+        }
+
+        private bool Fidelity20(int Travels) => Travels >= 20 ? true : false;
+
+        private bool Fidelity10(int Travels) => Travels >= 10 && Travels < 20 ? true : false;
     }
 }
--- UberFare/UberFare.Tests.cs
+++ UberFare/UberFare.Tests.cs
@@ -7,53 +7,89 @@ namespace UberFare
         [Fact]
         public void DayTimeFareForShortDistances()
         {
-            var price = new Price(1, 8);
+            var price = new Price(1, 8, 1, 1);
             int result = 5;            
-            var actual = price.CalculateUberFare(price);
+            var actual = price.CalculateUberFare();
 
             Assert.Equal(result, actual);
         }
         [Fact]
         public void DayTimeFareForMediumDistances()
         {
-            var price = new Price(21, 8);
+            var price = new Price(21, 8, 1, 1);
             int result = 168;
-            Assert.Equal(result, price.CalculateUberFare(price));
+            Assert.Equal(result, price.CalculateUberFare());
         }
         [Fact]
         public void DayTimeFareForLongDistances()
         {
-            var price = new Price(100, 8);
+            var price = new Price(100, 8, 1, 1);
             int result = 600;
-            Assert.Equal(result, price.CalculateUberFare(price));
+            Assert.Equal(result, price.CalculateUberFare());
         }
         [Fact]
         public void NightTimeFareForShortDistances()
         {
-            var price = new Price(1, 21);
+            var price = new Price(1, 21, 1, 1);
             int result = 7;
-            Assert.Equal(result, price.CalculateUberFare(price));
+            Assert.Equal(result, price.CalculateUberFare());
         }
         [Fact]
         public void NightTimeFareForMediumDistances()
         {
-            var price = new Price(21, 21);
+            var price = new Price(21, 21, 1, 1);
             int result = 210;
-            Assert.Equal(result, price.CalculateUberFare(price));
+            Assert.Equal(result, price.CalculateUberFare());
         }
         [Fact]
         public void NightTimeFareForLongDistances()
         {
-            var price = new Price(100, 21);
+            var price = new Price(100, 21, 1, 1);
             int result = 800;
-            Assert.Equal(result, price.CalculateUberFare(price));
+            Assert.Equal(result, price.CalculateUberFare());
         }
         [Fact]
         public void CheckNightTimeFareForLongDistances()
         {
-            var price = new Price(100, 21);
+            var price = new Price(100, 21, 1, 1);
             int result = 8;
-            Assert.Equal(result, price.GetPricePerKm(price));
+            Assert.Equal(result, price.GetPricePerKm());
+        }
+        [Fact]
+        public void SplitBillForNightTimeFareForLongDistances()
+        {
+            var price = new Price(100, 21, 4, 1);
+            decimal result = 200;
+            Assert.Equal(result, price.GetPricePerPerson());
+        }
+        [Fact]
+        public void RewardPriceToFidelRiders_ForNightTimeFareForLongDistances()
+        {
+            var price = new Price(100, 21, 1, 15);
+            decimal rewardPrice = (decimal)5.6;
+            Assert.Equal(rewardPrice, price.GetRewardPricePerKm());
+            decimal result = 560;
+            Assert.Equal(result, price.CalculateUberFare());
+        }
+        [Fact]
+        public void RewardPriceToFidelRiders_ForDayTimeFareForShortDistances()
+        {
+            var price = new Price(1, 8, 1, 15);
+            decimal rewardPrice = (decimal)4.2;
+            Assert.Equal(rewardPrice, price.GetRewardPricePerKm());
+            decimal result = (decimal)4.2;
+            Assert.Equal(result, price.CalculateUberFare());
+        }
+        [Fact]
+        public void SplitBill_RewardPriceToFidelRiders_ForDayTimeFareForShortDistances()
+        {
+            var price = new Price(10, 8, 2, 15);
+            decimal splitBill = 21;
+            Assert.Equal(splitBill, price.GetPricePerPerson());
+            decimal rewardPrice = (decimal)4.2;
+            Assert.Equal(rewardPrice, price.GetRewardPricePerKm());
+            decimal result = (decimal)42;
+            Assert.Equal(result, price.CalculateUberFare());
         }
     }
 }
